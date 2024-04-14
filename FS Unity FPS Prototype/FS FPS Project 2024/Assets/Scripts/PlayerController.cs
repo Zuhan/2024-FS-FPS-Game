83985b3c3d68e4c9f858cc39cd27fac31b87044a
apparitiongg <124656348+apparitiongg@users.noreply.github.com>
2024-04-13 23:13:01 -0400

    Player Prefab Creation
    Basic functionality for player movement.

---
+++ FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] int HP;
+    [SerializeField] int speed;
+    [SerializeField] int jumpHeight;
+    [SerializeField] int maxJumps;
+    [SerializeField] int gravity;
+
+    [SerializeField] int shootDamage;
+    [SerializeField] float shootRate;
+    [SerializeField] int shootDistance;
+
+    [SerializeField] GameObject cube;
+
+    Vector3 moveDirection;
+    Vector3 playerVelocity;
+    bool isShooting;
+    int jumpedTimes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDistance, Color.red);
+        movement();
+    }
+    
+    void movement()
+    {
+        if(controller.isGrounded)
+        {
+            jumpedTimes = 0;
+            playerVelocity = Vector3.zero;
+        }
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = (Input.GetAxis("Horizontal") * transform.right)
+                        + (Input.GetAxis("Vertical") * transform.forward);
+        controller.Move(moveDirection * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && jumpedTimes < maxJumps)
+        {
+            jumpedTimes++;
+            playerVelocity.y = jumpHeight;
+        }
+
+        playerVelocity.y -= gravity * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
