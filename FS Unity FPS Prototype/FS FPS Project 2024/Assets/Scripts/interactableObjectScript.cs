7e324a16e9efc6e4656fe4e73e041cd0df005c20
Ben Latta <138098366+Blatta17@users.noreply.github.com>
2024-04-15 21:36:05 -0400

    made the interact a little bit more polished

--- FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/interactableObjectScript.cs
+++ FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/interactableObjectScript.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class interactableObjectScript : MonoBehaviour, IfInteract
 {
+    [SerializeField] Renderer model;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,12 @@ public class interactableObjectScript : MonoBehaviour, IfInteract
     }
     public void interact()
     {
+        StartCoroutine(objInteract());
+    }
+    IEnumerator objInteract()
+    {
+        model.material.color = Color.green;
+        yield return new WaitForSeconds(1);
         Destroy(gameObject);
     }
 }
