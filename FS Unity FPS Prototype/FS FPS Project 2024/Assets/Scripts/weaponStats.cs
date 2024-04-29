bd6f27135e321f7d381283fd212e391248e48b64
Derek C Butler <cnc_master284@yahoo.com>
2024-04-28 20:08:00 -0700

    Minor Changes

--- FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/weaponStats.cs
+++ FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/weaponStats.cs
@@ -11,10 +11,9 @@ public class weaponStats : ScriptableObject
     public int castDamage;
     public float castRate;
     public int castDist;
+    public int cooldown;
 
     public ParticleSystem hitEffect;
     public AudioClip castSound;
     [Range(0, 1)] public float castSoundVol;
-
-
 }
