db7f958a5bdfe9b3ae0a526e029a98d6a986ff2c
apparitiongg <124656348+apparitiongg@users.noreply.github.com>
2024-05-02 02:34:13 -0400

    ?

---
+++ FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/Enemy scripts/Melee.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Melee : MonoBehaviour
+{
+
+    [SerializeField] int damage;
+    bool hitHappend;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.isTrigger)
+        {
+            return;
+        }
+
+        IDamage dmg = other.GetComponent<IDamage>();
+
+        if (dmg != null && !hitHappend)
+        {
+            dmg.TakeDamage(damage);
+            hitHappend = true;
+        }
+        hitHappend = false;
+    }
+
+}
