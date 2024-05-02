89239607ef88a82748262cf0fc1437a1b9abec38
Ben Latta <138098366+Blatta17@users.noreply.github.com>
2024-05-02 02:09:15 -0400

    saving weapons between scenes

---
+++ FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/playerStats.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class playerStats
+{
+    public static List<weaponStats> weapons = new List<weaponStats>();
+}
