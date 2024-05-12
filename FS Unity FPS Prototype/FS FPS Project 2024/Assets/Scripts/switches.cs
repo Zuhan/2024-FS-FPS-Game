507a503414b8faa0bc702300de720e7c5c6c8a04
Ben Latta <138098366+Blatta17@users.noreply.github.com>
2024-05-12 19:56:19 -0400

    new kind of door
    door that requires switches to be pressed added, only opens when all switches are pressed.
    The basic stuff is there, it needs some polish

---
+++ FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/switches.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switches : MonoBehaviour, IfInteract
+{
+    private bool switchedOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switchedOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void interact()
+    {
+        if (!switchedOn)
+        {
+            StartCoroutine(switchOn());
+            switchedOn = true;
+        }
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        
+    }
+    public bool isSwitchedOn()
+    {
+        return switchedOn;
+    }
+    IEnumerator switchOn()
+    {
+        //do something here
+        yield return new WaitForSeconds(1f);
+    }
+}
