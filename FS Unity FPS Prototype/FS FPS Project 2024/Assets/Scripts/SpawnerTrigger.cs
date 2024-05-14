c75c3927d0f9b413a751bcb265942e91058bdaf5
Ben Latta <138098366+Blatta17@users.noreply.github.com>
2024-05-14 04:27:26 -0400

    modifiable weights on spawner
    spawner can now be weighted more easily through serialize fields

--- FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/SpawnerTrigger.cs
+++ FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/SpawnerTrigger.cs
@@ -7,7 +7,12 @@ public class SpawnerTrigger : MonoBehaviour
     [SerializeField] GameObject[] enemiesToSpawn;
     [SerializeField] int numToSpawn;
     [SerializeField] int spawnDelay;
-
+    [Header("--- Weighting modifiers ---(Adds to 100, 1 value is missing for the mimic, its just the remainder of the 100 -  the 5 numbers)")]
+    [SerializeField] int miniSkeleWeight1;
+    [SerializeField] int miniSkeleWeight2;
+    [SerializeField] int miniSkeleWeight3;
+    [SerializeField] int beholderWeight;
+    [SerializeField] int ArmoredSkeleWeight;
     int spawnCount;
     bool isSpawning;
     bool startSpawning;
@@ -44,20 +49,20 @@ public class SpawnerTrigger : MonoBehaviour
     }
 
     //algorithim to spawn enemies in a weighted way to make enemies less likely or more likely to spawn
-    //not yet implemented fully
+    //if enemies are removed from the list, it will fall back to the first enemy in the list as a default enemy to spawn
     private int enemyAlgorithim()
     {
         int enemy = Random.Range(0, 100);
         Debug.Log(enemy);
         int num = 0;
         //spawn case 1: mini skele
-        if(enemy <= 19)
+        if(enemy <= miniSkeleWeight1-1)
         {
             num = 0;
             return num;
         }
         //spawn case 2: skele red mage
-        else if(enemy <= 39)
+        else if(enemy <= miniSkeleWeight2-1+miniSkeleWeight1)
         {
             num = 1;
             if (num >= enemiesToSpawn.Length)
@@ -67,7 +72,7 @@ public class SpawnerTrigger : MonoBehaviour
             return num;
         }
         //spawn case 3: skele yellow bomber
-        else if (enemy <= 59)
+        else if (enemy <= miniSkeleWeight3+miniSkeleWeight2+miniSkeleWeight1-1)
         {
             num = 2;
             if (num >= enemiesToSpawn.Length)
@@ -77,7 +82,7 @@ public class SpawnerTrigger : MonoBehaviour
             return num;
         }
         //spawn case 4: beholder
-        else if (enemy <= 79)
+        else if (enemy <= miniSkeleWeight1+miniSkeleWeight2+miniSkeleWeight3+beholderWeight-1)
         {
             num = 3;
             if (num >= enemiesToSpawn.Length)
@@ -87,7 +92,7 @@ public class SpawnerTrigger : MonoBehaviour
             return num;
         }
         //spawn case 5: armored melee skele
-        else if (enemy <= 89)
+        else if (enemy <= miniSkeleWeight1 + miniSkeleWeight2 + miniSkeleWeight3 + beholderWeight + ArmoredSkeleWeight-1)
         {
             num = 4;
             if (num >= enemiesToSpawn.Length)
