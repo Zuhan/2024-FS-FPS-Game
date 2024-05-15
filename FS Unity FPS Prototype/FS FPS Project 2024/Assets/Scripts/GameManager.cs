4641556a18d9fd5b6600332172fd5fe1e82ee42b
Derek C Butler <cnc_master284@yahoo.com>
2024-05-15 07:21:34 -0700

    Small UI Update

--- FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/GameManager.cs
+++ FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/GameManager.cs
@@ -54,18 +54,6 @@ public class gameManager : MonoBehaviour
     //Cooldown Ring Added by Derek
     public GameObject cooldownRing;
 
-    //Explosion Being Shelved
-    ////Explosion Staff prefab added by Derek
-    //public GameObject explosionStaffPrefab;
-    ////Reference to the explosion staff object added by Derek
-    //private GameObject explosionStaffObject;
-    ////Reference to the explosion staff script added by Derek
-    //private explosionStaff explosionStaffScript;
-    ////Explosion Magic prefab added by Derek
-    //public GameObject ExplosionMagicPrefab;
-    ////Nuclear Explosion prefab added by Derek
-    //public GameObject Explosion;
-
 
     //enemy count field 
     public int enemyCount;
@@ -85,6 +73,7 @@ public class gameManager : MonoBehaviour
     public bool slingUI;
     public bool fireUI;
     public bool thUI;
+    public bool exploUI;
     //void awake so its called first 
     void Awake()
     {
@@ -248,21 +237,7 @@ public class gameManager : MonoBehaviour
                 stateUnpaused();
                 invOpen = false;
             }
-        }
-
-        //Explosion Staff Being Shelved
-        //Equipping the Explosion_Staff by pressing 0 key
-        //if (Input.GetKeyDown(KeyCode.Alpha0))
-        //{
-        //    if (explosionStaffObject != null)
-        //    {
-        //        UnequipExplosionStaff();
-        //    }
-        //    else
-        //    {
-        //        EquipExplosionStaff();
-        //    }
-        //}        
+        }      
     }
 
     public void ShowWeaponIcon(int weaponIndex)
@@ -283,53 +258,4 @@ public class gameManager : MonoBehaviour
             playerStats.thUI = true;
         }
     }
-    //Explosion Staff Being Shelved
-    //Unequipping the Explosion_Staff by pressing the 1 key added by Derek
-    //public void UnequipExplosionStaff()
-    //{
-    //    if (explosionStaffObject != null)
-    //    {
-    //        Destroy(explosionStaffObject);
-
-    //        explosionStaffObject = null;
-    //        explosionStaffScript = null;
-    //    }
-    //}
-    // Method for equipping any type of weapon after ensuring no other weapon is already equipped
-    //private void EquipWeaponSafely(GameObject weaponPrefab, Vector3 offset, Quaternion rotation)
-    //{
-    //    if (Camera.main.transform.childCount > 0)
-    //    {
-    //        // Destroy the currently equipped weapon
-    //        Destroy(Camera.main.transform.GetChild(0).gameObject);
-    //    }
-
-    //    // Instantiate and equip the new weapon
-    //    GameObject equippedWeapon = Instantiate(weaponPrefab, Vector3.zero, Quaternion.identity);
-    //    equippedWeapon.transform.parent = Camera.main.transform;
-    //    equippedWeapon.transform.localPosition = offset;
-    //    equippedWeapon.transform.rotation = rotation;
-    //}
-
-    //Explosion Staff Being Shelved
-    //Method for equipping Explosion_Staff prefab added by Derek
-    //public void EquipExplosionStaff()
-    //{
-    //    if (explosionStaffObject == null)
-    //    {
-    //        Vector3 offset = new Vector3(0.5f, -1.2f, -0.3f); // X offset, Y offset, Z offset
-
-    //        Quaternion rotation = Quaternion.LookRotation(Camera.main.transform.forward, Camera.main.transform.up);
-    //        rotation *= Quaternion.Euler(-45f, 180f, 0f);
-
-    //        EquipWeaponSafely(explosionStaffPrefab, offset, rotation);
-
-    //        explosionStaffObject = Camera.main.transform.GetChild(0).gameObject;
-    //        explosionStaffScript = explosionStaffObject.GetComponent<explosionStaff>();
-    //    }
-    //    else
-    //    {
-    //        UnequipExplosionStaff();
-    //    }
-    //}
 }
