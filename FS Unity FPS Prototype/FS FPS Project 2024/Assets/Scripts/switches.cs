dd5ffa70dce6747862a00ed4a16ae09e1f1e34e9
Ben Latta <138098366+Blatta17@users.noreply.github.com>
2024-05-20 04:02:00 -0400

    switch modeled and animated

--- FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/switches.cs
+++ FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/switches.cs
@@ -12,6 +12,7 @@ public class switches : MonoBehaviour, IfInteract
     private bool switchedOn;
     [SerializeField] AudioClip audSwitch;
     [Range(0, 1)][SerializeField] float audSwitchVol;
+    bool rotating;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +29,7 @@ public class switches : MonoBehaviour, IfInteract
         if (!switchedOn)
         {
             StartCoroutine(switchOn());
+            StartCoroutine(rotate(new Vector3(-90,0,0),1));
             switchedOn = true;
         }
     }
@@ -48,6 +50,24 @@ public class switches : MonoBehaviour, IfInteract
         aud.PlayOneShot(audSwitch,audSwitchVol);
         menu.SetActive(false);
         switchDoor.UpdateSwitch();
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0f);
+    }
+    IEnumerator rotate(Vector3 rot,float dur)
+    {
+        if (rotating)
+        {
+            yield break;
+        }
+        rotating = true;
+        Vector3 newRot = transform.eulerAngles + rot;
+        Vector3 curRot = transform.eulerAngles;
+        float count = 0;
+        while(count < dur)
+        {
+            count += Time.deltaTime;
+            transform.eulerAngles = Vector3.Lerp(curRot,newRot,count);
+            yield return null;
+        }
+        rotating = false;
     }
 }
