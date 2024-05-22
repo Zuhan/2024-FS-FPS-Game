329442a0d6af8e65683f1c1830fb28f17104f57d
Ben Latta <138098366+Blatta17@users.noreply.github.com>
2024-05-22 05:16:55 -0400

    added vendors for all weapons
    - you don't have to use all of them but the functionality is there so i added them

---
+++ FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/expStaffBuy.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class expStaffBuy : MonoBehaviour, IfInteract
+{
+    [Header("---Fields for audio---")]
+    [SerializeField] AudioSource aud;
+    [SerializeField] AudioClip audBuy;
+    [Range(0, 1)][SerializeField] float audBuyVol;
+    [Header("---Fields for passing potion object and cost---")]
+    [SerializeField] GameObject weapon;
+    [SerializeField] int cost;
+    [Header("---Fields for UI elements---")]
+    [SerializeField] GameObject menuText;
+    [SerializeField] GameObject menuText2;
+    public TMP_Text costText;
+    [SerializeField] GameObject failText;
+    [SerializeField] GameObject soldOutText;
+
+    private bool soldOut;
+    // Start is called before the first frame update
+    void Start()
+    {
+        costText.text = cost.ToString();
+    }
+    public void interact()
+    {
+        if(gameManager.instance.points >= cost && !soldOut)
+        {
+            Instantiate(weapon,gameManager.instance.playerScript.transform.position, new Quaternion());
+            gameManager.instance.pointsChange(-cost);
+            playSound();
+            soldOut = true;
+            displaySoldOut();
+        }
+        else if (soldOut)
+        {
+            displaySoldOut();
+        }
+        else
+        {
+            StartCoroutine(displayFail());
+        }
+    }
+    IEnumerator displayFail()
+    {
+        menuText.SetActive(false);
+        menuText2.SetActive(false);
+        failText.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        failText.SetActive(false);
+        menuText.SetActive(true);
+        menuText2.SetActive(true);
+    }
+    private void playSound()
+    {
+        aud.PlayOneShot(audBuy, audBuyVol);
+    }
+    private void displaySoldOut()
+    {
+        menuText.SetActive(false);
+        menuText2.SetActive(false);
+        soldOutText.SetActive(true);
+    }
+}
