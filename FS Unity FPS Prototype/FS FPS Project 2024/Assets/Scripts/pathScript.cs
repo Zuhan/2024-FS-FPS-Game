e2e9ebb6f2b231c0618fdc5886756e53fcb8fffa
Derek C Butler <cnc_master284@yahoo.com>
2024-06-02 10:24:29 -0700

    Game Space Cleanup

--- FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/pathScript.cs
+++ FS Unity FPS Prototype/FS FPS Project 2024/Assets/Scripts/pathScript.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class pathScript : MonoBehaviour
 {
-    //read me
-    //you need a path to attach to the moving platform
-    //the path prefab has this script attached
-    //attach the path object to the serialize field on the moving platform
-    //you can move the path anywhere and change how many points in the path there are
     public Transform getPoint(int index)
     {
         return transform.GetChild(index);
